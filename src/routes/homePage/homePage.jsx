bb34fe56bf4e6d38329b5df988e5ee54f56c5589
Chandula Samarasinghe <Chandulas1999@gmail.com>
2024-09-18 00:03:13 +0530

    finalized the left side of the home page

---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,26 @@
+import "./homePage.scss";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <div className="title">
+            <h1>Find Real State and Find Build Dream Place</h1>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+            aperiam quisquam autem ratione dolores. Possimus, perferendis
+            voluptate repudiandae inventore nisi repellat, amet placeat minima
+            recusandae porro, facere velit commodi enim.
+          </p>
+        </div>
+      </div>
+      <div className="imageContainer">
+        <img src="./bg.png" alt="Image" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
