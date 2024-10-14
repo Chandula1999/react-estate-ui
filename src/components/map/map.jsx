e2213789711d7be5d85445e463ddbd46051535e3
Chandula Samarasinghe <Chandulas1999@gmail.com>
2024-10-15 01:17:04 +0530

    Refactor import statement for Map component in map.jsx

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,11 +1,13 @@
+import { MapContainer, TileLayer } from "react-leaflet";
 import "./map.scss";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-function Map() {
+import Pin from "../pin/Pin";
+
+function Map({ items }) {
   return (
     <MapContainer
-      center={[52.4797, -1.30269]}
-      zoom={13}
+      center={[52.4797, -1.90269]}
+      zoom={7}
       scrollWheelZoom={false}
       className="map"
     >
@@ -13,11 +15,9 @@ function Map() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[52.4797, -1.90269]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {items.map((item) => (
+        <Pin item={item} key={item.id} />
+      ))}
     </MapContainer>
   );
 }
