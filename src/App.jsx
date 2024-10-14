7613c31785ca165dc7efca144ab9fcce9e5ea4c3
Chandula Samarasinghe <Chandulas1999@gmail.com>
2024-10-15 01:19:37 +0530

    Refactor routing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
-import Navbar from "./components/navbar/Navbar.jsx";
+import ListPage from "./routes/listPages/listPage.jsx";
 import HomePage from "./routes/homePage/homePage.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./routes/layout/layout.jsx";
+import SinglePage from "./routes/singlePage/singlePage.jsx";
+import ProfilePage from "./routes/profilePage/profilePage.jsx";
+import Register from "./routes/register/register.jsx";
+import LoginPage from "./routes/loginPage/loginPage.jsx";
+
 function App() {
-  return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage />,
+        },
+        {
+          path: "/login",
+          element: <LoginPage />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
